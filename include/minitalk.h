15a399f9be921857db45568b2aa54d1032d97a90
bmoreira <bmoreira@student.42sp.org.br>
2025-09-09 20:58:58 -0300

    initial structure with sigaction

--- include/minitalk.h
+++ include/minitalk.h
@@ -6,15 +6,16 @@
 /*   By: bmoreira <bmoreira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 19:42:02 by bmoreira          #+#    #+#             */
-/*   Updated: 2025/09/08 20:59:12 by bmoreira         ###   ########.fr       */
+/*   Updated: 2025/09/09 20:49:38 by bmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
+# include "../libft/include/libft.h"
 # include <stdlib.h>
 # include <unistd.h>
-# include "../libft/include/libft.h"
+# include <signal.h>
 
 #endif
--- src/client.c
+++ src/client.c
@@ -6,13 +6,21 @@
 /*   By: bmoreira <bmoreira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 19:45:46 by bmoreira          #+#    #+#             */
-/*   Updated: 2025/09/08 21:01:05 by bmoreira         ###   ########.fr       */
+/*   Updated: 2025/09/09 20:28:53 by bmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
 
+void sig_handler(int sig, siginfo_t *info, void *context)
+{
+	ft_printf("Receiving signal %d from PID: %d", sig, info->si_pid);
+}
+
 int	main(int argc, char *argv[])
 {
-	ft_printf("argc: %d, argv: %s", argc, argv[1]);
+	struct sigaction sa;
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = sig_handler;
+	sigemptyset(&sa.sa_mask);
 }
