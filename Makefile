b29e731d9d4211a7cd5f6f10a70006ab62830daf
bmoreira <bmoreira@student.42sp.org.br>
2025-09-08 21:03:27 -0300

    started project. created makefile, header and initial prototypes. included libft.

---
+++ Makefile
@@ -0,0 +1,44 @@
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+INCLUDES = -I./include/ -I$(LIBFT_DIR)
+
+LIBFT_DIR = libft
+LIBFT = $(LIBFT_DIR)/libft.a
+
+SRC_DIR = src/
+SRC = teste.c
+OBJ_DIR = objs/
+OBJ = $(SRC:%.c=$(OBJ_DIR)%.o)
+
+C_SRC = client.c
+C_OBJ = $(C_SRC:%.c=$(OBJ_DIR)%.o)
+C_NAME = client
+
+S_SRC = server.c
+S_OBJ = $(S_SRC:%.c=$(OBJ_DIR)%.o)
+S_NAME = server
+
+all: $(LIBFT) $(C_NAME) $(S_NAME)
+
+$(C_NAME): $(C_OBJ) $(OBJ)
+	$(CC) $(C_OBJ) $(OBJ) $(LIBFT) $(INCLUDES) -o $(C_NAME)
+
+$(S_NAME): $(S_OBJ) $(OBJ)
+	$(CC) $(S_OBJ) $(OBJ) $(LIBFT) $(INCLUDES) -o $(S_NAME)
+
+$(OBJ_DIR)%.o: $(SRC_DIR)%.c
+	mkdir -p $(dir $@)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+$(LIBFT):
+	make -C $(LIBFT_DIR)
+
+clean:
+	rm -rf $(OBJ_DIR)
+	make -C $(LIBFT_DIR) clean
+
+fclean: clean
+	rm -f $(C_NAME) $(S_NAME)
+	make -C $(LIBFT_DIR) fclean
+
+re: fclean all
---
+++ include/minitalk.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmoreira <bmoreira@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/08 19:42:02 by bmoreira          #+#    #+#             */
+/*   Updated: 2025/09/08 20:59:12 by bmoreira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include "../libft/include/libft.h"
+
+#endif
